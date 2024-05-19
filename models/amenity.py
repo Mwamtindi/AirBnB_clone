c788230eb7b98f7182c324d427b754c00a5b0b57
Mwamtindi <mwamtindishaban@gmail.com>
2024-05-19 21:13:00 +0300

    amenity class

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """attribute:
+        name : (empty str) reprsent the name of the amenity.
+    """
+
+    name = ""
