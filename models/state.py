92fd280417b3f5e2dba70f550ad4bf233091f42e
Mwamtindi <mwamtindishaban@gmail.com>
2024-05-19 21:17:14 +0300

    state class

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """attribute:
+        name : (empty str)represent the name of the state.
+    """
+
+    name = ""
